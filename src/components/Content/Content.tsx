41423a3d9373d8ce415f65212b46901d5a050add
Aryadne <Aryadne@PAULO-CESAR>
2026-04-16 14:45:18 -0300

    Finalized banner design.

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,8 +1,20 @@
+import style from './Content.module.css';
+
 const Content = () => {
     return (
-        <h2>
-            Content
-        </h2>
+        <div className={style.Content}>
+            <div className={style.banner}>
+                <img src='src\assets\img-home.png' alt='home' className={style.bannerImg} />
+                <div className={style.bannerContent}>
+                    <h1><span>NEO GENESIS:</span> RISING</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <div className={style.buttons}>
+                        <button className={style.btnRosa}>ASSISTIR AGORA</button>
+                        <button className={style.btnVazado}>+ MINHA LISTA</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
